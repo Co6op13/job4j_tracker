c4618744d64016f6278519ba9150e69b42057668
Co6op13 <Co6op13@yandex.ru>
2022-08-25 09:44:58 +0500

    Массивы и модели

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -1,22 +1,43 @@
 package ru.job4j.pojo;
 
 public class Library {
+
+    public static void printArray(Book[] books) {
+        for (Book book : books) {
+            System.out.println(book.getName() + " - " + book.getPageCount());
+        }
+        System.out.println();
+    }
+
+    public static void findBook(Book[] books, String name) {
+        for (Book book : books) {
+            if (name.equals(book.getName())) {
+                System.out.println(book.getName() + " - " + book.getPageCount());
+            }
+        }
+        System.out.println();
+    }
+
+    private static void swapPlace(Book[] books, int first, int second) {
+        if (first < books.length && first >= 0 && second < books.length && second >= 0) {
+            var temp = books[first];
+            books[first] = books[second];
+            books[second] = temp;
+        } else {
+            System.out.println("неверный индекс. При замене произошла ошибка");
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Book[] books = new Book[4];
         books[0] = new Book("Head First Java", 722);
         books[1] = new Book("Clean code", 464);
         books[2] = new Book("Java 8. Полное руководство", 1375);
         books[3] = new Book("Расплетающие Сновидения", 496);
-        for (int i = 0; i < books.length; i++) {
-            System.out.println(books[i].getName() + " - " + books[i].getPageCount());
-        }
-        Book temp =  books[0];
-        books[0] = books[3];
-        books[3] = temp;
-        for (Book book : books) {
-            if (book.getName().equals("Clean code")) {
-                System.out.println(book.getName() + " - " + book.getPageCount());
-            }
-        }
+        printArray(books);
+        swapPlace(books, 0, 3);
+        printArray(books);
+        findBook(books, "Clean code");
     }
 }
