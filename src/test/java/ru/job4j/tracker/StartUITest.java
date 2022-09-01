ecad20eebd197099a9bede9290856f0c8393fe9e
co6op13 <Co6op13@yandex.ru>
2022-09-01 09:15:16 +0300

    Тестирование. Подготовка данных

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -35,8 +35,7 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
+                String.valueOf(item.getId())
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
